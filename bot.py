08cf6a50a95659108a5c33b7370d81a3f08d4d2f
Phil <philine.bargen@stud-mail.uni-wuerzburg.de>
2023-08-22 21:54:49 +0200

    Sprüche funktioniere, sowie audio und files senden Es fehlt nur noch der Timer + clean up

--- bot.py
+++ bot.py
@@ -5,9 +5,13 @@ from dataclasses import dataclass
 from strsimpy.levenshtein import Levenshtein
 import yaml
 from typing import List
+from random import randint
+from discord.ext import commands
 
-configFile = open("config.yaml", 'r')
+configFile = open("config.yaml", 'r', encoding='UTF-8')
 config = yaml.safe_load(configFile)
+spruchFile = open("spruch.yaml", 'r', encoding='UTF-8')
+spruch = yaml.safe_load(spruchFile)
 
 simAlg = Levenshtein()
 
@@ -58,10 +62,14 @@ async def on_ready():
     for team in config['Teams']:
         teamList.append(TeamData(**team))
 
+    # for team in teamList:
+    #     channel = await guild.fetch_channel(team.questID)
+    #     await channel.send('**Aufgabe 1 - Classic:**')
+    #     await channel.send(config['AUFGABE1'])
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user or message.author.id == 472509126010994689:
         return
 
     user = message.author
@@ -74,24 +82,39 @@ async def on_message(message):
                 userTeam = team
 
     if message.channel.id == userTeam.answerID:
+        await message.channel.typing()
         await switch(str(message.content), userTeam, message.channel)
 
 
 async def switch(answer, userTeam, channel):
     puzzleNr = userTeam.currentPuzzle
     if puzzleNr != '10':
+        # distance kann keine Umlaute oder ß
         distance = simAlg.distance(str(config['Puzzles']['ANTWORT' + str(puzzleNr)]), answer.lower())
-        print(distance)
         if distance == 0:
             await channel.send(f'{answer} ist richtig!')
+
             userTeam.currentPuzzle += 1
+
             # nächstes Puzzle schicken
+            for aufgabe in config['AUFGABE' + str(userTeam.currentPuzzle)]:
+                questChannel = await guild.fetch_channel(userTeam.questID)
+                await questChannel.send(str(aufgabe))
+
+            if userTeam.currentPuzzle == 6:
+                #await channel.send(file=discord.File(r'C:\Users\phili\Pictures\0_PuzzleHunt\6_LU17\Genesis.mp3'))
+                await questChannel.send(file=discord.File(r'C:\Users\phili\Music\Youtube\Dune_OST_–_Main_Theme_Suite_ _Hans_Zimmer.mp3'))
+                await questChannel.send(file=discord.File(r'C:\Users\phili\Documents\PnP\blood on the clocktower\bad-moon-rising.character-reference.pdf'))
+
         elif distance <= round(len(answer) * 0.2):
             await channel.send(f'{answer} ist nah dran!')
-            await channel.send(f'Wert: {distance}')
         else:
-            await channel.send(f'Deine Mudda ist {answer}')
-            await channel.send(f'Wert: {distance}')
+            if (puzzleNr == 5 and config['Zwischenantworten']['ZA5'] == answer.lower()) or (puzzleNr == 8 and config['Zwischenantworten']['ZA8'] == answer.lower()):
+                await channel.send(f'Das ist korrekt, aber noch nicht ganz die Lösung...')
+                return
+            else:
+                zufallszahl = randint(1, 24)
+                await channel.send(spruch['SPRUCH'+ str(zufallszahl)].format(answer))
 
 
 client.run(token)
